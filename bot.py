e57f1a11ba541e61f157d424cef96c1ea1d11d84
Mubeen <patel.mubeen99@gmail.com>
2019-04-26 12:24:43 +0530

    final changes

--- bot.py
+++ bot.py
@@ -3,13 +3,15 @@ import pyautogui
 import time
 from numpy import *
 
+
 class Cordinates():
-    replayBtn = (340,390)
-    dinasaur = (171,395)
+    replayBtn = (340, 390)
+    dinasaur = (171, 395)
 
 
 def restartGame():
     pyautogui.click(Cordinates.replayBtn)
+
 
 def pressSpace():
     pyautogui.keyDown('space')
@@ -17,18 +19,22 @@ def pressSpace():
     print('Jump')
     pyautogui.keyUp('space')
 
+
 def imageGrab():
-    box = (Cordinates.dinasaur[0]+50,Cordinates.dinasaur[1],Cordinates.dinasaur[0]+100, Cordinates.dinasaur[1]+30)
+    box = (Cordinates.dinasaur[0]+50, Cordinates.dinasaur[1], Cordinates.dinasaur[0]+100, Cordinates.dinasaur[1]+30)
     image = ImageGrab.grab(box)
     grayImage = ImageOps.grayscale(image)
     a = array(grayImage.getcolors())
     print(a.sum())
     return a.sum()
+
+
 def main():
     restartGame()
     while True:
-        if(imageGrab()!=1747):
+        if imageGrab() != 1747:
             pressSpace()
             time.sleep(0.1)
+
 
 main()
